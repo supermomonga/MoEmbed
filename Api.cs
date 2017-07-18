03f1efcfd5207a66bc1a4ce39c07e8cb37adf0e9
supermomonga <hi@supermomonga.com>
2017-07-19 04:29:10 +0900

    hello world

---
+++ Api.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MoEmbed
+{
+    public class Api
+    {
+        public static Task Index(HttpContext context)
+        {
+            return context.Response.WriteAsync("Hello MoEmbed");
+        }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -16,6 +17,7 @@ namespace MoEmbed
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRouting();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,10 +30,11 @@ namespace MoEmbed
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            var routeBuilder = new RouteBuilder(app);
+            routeBuilder.MapGet("", MoEmbed.Api.Index);
+
+            app.UseRouter(routeBuilder.Build());
         }
     }
 }
+
