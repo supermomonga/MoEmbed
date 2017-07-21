4016c09d8e7861eb5f38475bec54066d1b75d260
pgrho <shipwreck@live.jp>
2017-07-21 09:57:58 +0900

    Fix ResponseWriterExtension to write int? as JSON number

--- MoEmbed/Models/ResponseWriterExtension.cs
+++ MoEmbed/Models/ResponseWriterExtension.cs
@@ -22,7 +22,7 @@ namespace MoEmbed.Models
         {
             if(value != null)
             {
-                writer.WriteProperty(name, value);
+                writer.WriteProperty(name, value.Value);
             }
         }
     }
