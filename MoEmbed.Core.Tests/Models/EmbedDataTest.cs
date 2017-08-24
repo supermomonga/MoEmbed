1b77dc7a440b1e0a5503afadd99f26cd1b08db04
pgrho <shipwreck@live.jp>
2017-08-24 15:18:15 +0900

    Added EmbedDataTest

---
+++ MoEmbed.Core.Tests/Models/EmbedDataTest.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Portable.Xaml;
+using Xunit;
+
+namespace MoEmbed.Models
+{
+    public class EmbedDataTest
+    {
+        [Fact]
+        public void SerializationTest()
+        {
+            // TODO: Update Portable.Xaml
+            var expected = new EmbedData()
+            {
+                // Description = "{NotBinding}",
+                CacheAge = null,
+                // AuthorName = "{}{Author}"
+            };
+
+            using (var sw = new StringWriter())
+            {
+                XamlServices.Save(sw, expected);
+
+                var xml = sw.ToString();
+                using (var sr = new StringReader(xml))
+                {
+                    var obj = XamlServices.Load(sr);
+                    Assert.IsType<EmbedData>(obj);
+
+                    var actual = (EmbedData)obj;
+
+                    Assert.Equal(expected.Description, actual.Description);
+                    Assert.Equal(expected.CacheAge, actual.CacheAge);
+                    Assert.Equal(expected.AuthorName, actual.AuthorName);
+                }
+            }
+        }
+    }
+}
