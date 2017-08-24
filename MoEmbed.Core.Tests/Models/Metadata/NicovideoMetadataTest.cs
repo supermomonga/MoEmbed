9a83fd495f1040c043828de418eef4bf573cd574
pgrho <shipwreck@live.jp>
2017-08-24 21:27:18 +0900

    Test NicovideoMetadataProvider

---
+++ MoEmbed.Core.Tests/Models/Metadata/NicovideoMetadataTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using Portable.Xaml;
+using Xunit;
+
+namespace MoEmbed.Models.Metadata
+{
+    public class NicovideoMetadataTest
+    {
+        [Fact]
+        public void SerializationTest()
+        {
+            var expected = new NicovideoMetadata()
+            {
+                VideoId = int.MaxValue + 1L,
+                Data = new EmbedData()
+                {
+                    Title = "hoge"
+                }
+            };
+
+            using (var sw = new StringWriter())
+            {
+                XamlServices.Save(sw, expected);
+
+                var xml = sw.ToString();
+                using (var sr = new StringReader(xml))
+                {
+                    var obj = XamlServices.Load(sr);
+                    Assert.IsType<NicovideoMetadata>(obj);
+
+                    var actual = (NicovideoMetadata)obj;
+
+                    Assert.Equal(expected.VideoId, actual.VideoId);
+                    Assert.Equal(expected.Data.Title, actual.Data.Title);
+                }
+            }
+        }
+
+        [Theory]
+        [InlineData(28278558L, "【プリパラ】「ぷりっとぱ～ふぇくと」をぬるぬるにしてみた【HD60fps】")]
+        public void FetchAsyncTest(long videoId, string expectedTitle)
+        {
+            var target = new NicovideoMetadata()
+            {
+                VideoId = videoId
+            };
+
+            var data = target.FetchAsync(
+                            new RequestContext(
+                                new MetadataService(),
+                                new ConsumerRequest(new Uri("http://www.nicovideo.jp/watch/sm" + videoId))))
+                                .GetAwaiter().GetResult();
+
+            Assert.Equal(expectedTitle, data.Title);
+        }
+    }
+}
---
+++ MoEmbed.Core.Tests/Providers/NicovideoMetadataProviderTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MoEmbed.Models;
+using MoEmbed.Models.Metadata;
+using Xunit;
+
+namespace MoEmbed.Providers
+{
+    public sealed class NicovideoMetadataProviderTest
+    {
+        #region CanHandle
+
+        [Theory]
+        [InlineData("http://www.nicovideo.jp/watch/sm43")]
+        public void CanHandleTest_True(string uri)
+            => Assert.True(new NicovideoMetadataProvider().CanHandle(new ConsumerRequest(new Uri(uri))));
+
+        [Theory]
+        [InlineData("https://rms.sexy/")]
+        public void CanHandleTest_False(string uri)
+            => Assert.False(new NicovideoMetadataProvider().CanHandle(new ConsumerRequest(new Uri(uri))));
+
+        #endregion CanHandle
+
+        #region GetMetadata
+
+        [Theory]
+        [InlineData("http://www.nicovideo.jp/watch/sm43", 43L)]
+        public void GetMetadataTest_Success(string uri, long videoId)
+        {
+            var m = Assert.IsType<NicovideoMetadata>(new NicovideoMetadataProvider().GetMetadata(new ConsumerRequest(new Uri(uri))));
+
+            Assert.Equal(videoId, m.VideoId);
+            Assert.Null(m.Data);
+        }
+
+        [Theory]
+        [InlineData("https://rms.sexy/")]
+        public void GetMetadataTest_Null(string uri)
+            => Assert.Null(new NicovideoMetadataProvider().GetMetadata(new ConsumerRequest(new Uri(uri))));
+
+        #endregion GetMetadata
+    }
+}
