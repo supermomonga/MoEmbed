a01fe7848a7cec9d0acfafed311aefc4b8b0470a
pgrho <shipwreck@live.jp>
2017-08-24 09:41:09 +0900

    Defined MetadataService.HttpClient

--- MoEmbed.Core/MetadataService.cs
+++ MoEmbed.Core/MetadataService.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using MoEmbed.Models;
 using MoEmbed.Providers;
@@ -10,7 +14,7 @@ namespace MoEmbed
     /// <summary>
     ///   Handles the request object and use right metadata handler to fetch embed data.
     /// </summary>
-    public class MetadataService
+    public class MetadataService : IDisposable
     {
         private readonly ILogger<MetadataService> _logger;
         private readonly IMetadataCache _Cache;
@@ -74,5 +78,71 @@ namespace MoEmbed
                 ErrorMessage = "Not Found"
             };
         }
+
+        #region HttpClient
+
+        private HttpClient _HttpClient;
+
+        /// <summary>
+        /// Gets a <see cref="HttpClient"/> instance that is shared over this <see cref="MetadataService"/>.
+        /// </summary>
+        /// <remarks>
+        /// The <see cref="HttpClient"/> won't follow redirect automatically.
+        /// </remarks>
+        public HttpClient HttpClient
+        {
+            get
+            {
+                if (_HttpClient == null)
+                {
+                    var c = new HttpClient(new HttpClientHandler()
+                    {
+                        AllowAutoRedirect = false,
+                    });
+
+                    // Set User-Agent
+
+                    // HACK: make User-Agent configurable
+                    const string product = "MoEmbed";
+                    var version = new AssemblyName(typeof(MetadataService).AssemblyQualifiedName).Version;
+                    const string url = "https://github.com/supermomonga/MoEmbed";
+
+                    c.DefaultRequestHeaders.UserAgent.Clear();
+                    c.DefaultRequestHeaders.UserAgent.Add(
+                        new ProductInfoHeaderValue($"Mozilla/5.0 (compatible; {product}/{version}; +{url})"));
+
+                    _HttpClient = c;
+                }
+                return _HttpClient;
+            }
+        }
+
+        #endregion HttpClient
+
+        #region IDisposable support
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        /// <inheritdoc />
+        ~MetadataService()
+            => Dispose(false);
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _HttpClient?.Dispose();
+            }
+            _HttpClient = null;
+        }
+
+        #endregion IDisposable support
     }
 }
