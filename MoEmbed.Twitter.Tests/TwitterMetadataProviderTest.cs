936123d375d9bd8034da6c96764c39314b71514e
supermomonga <hi@supermomonga.com>
2017-08-25 17:04:47 +0900

    Add screenName correction test

--- MoEmbed.Twitter.Tests/TwitterMetadataProviderTest.cs
+++ MoEmbed.Twitter.Tests/TwitterMetadataProviderTest.cs
@@ -17,11 +17,23 @@ namespace MoEmbed.Providers
                 .AddUserSecrets<TwitterMetadataProviderTest>()
                 .Build());
 
+        internal MetadataService _Service;
+        public MetadataService Service => _Service ?? ( _Service = new MetadataService() );
+
         public string ConsumerKey => Configuration["TwitterConsumerKey"] ?? Environment.GetEnvironmentVariable("TWITTER_CONSUMER_KEY");
         public string ConsumerSecret => Configuration["TwitterConsumerSecret"] ?? Environment.GetEnvironmentVariable("TWITTER_CONSUMER_SECRET");
 
         internal TwitterMetadataProvider _Provider;
         public TwitterMetadataProvider Provider => _Provider ?? ( _Provider = new TwitterMetadataProvider(ConsumerKey, ConsumerSecret) );
+
+        public RequestContext GetRequestContext(Uri url)
+        {
+            var consumerRequest = new ConsumerRequest(url);
+            return new RequestContext(Service, consumerRequest);
+        }
+
+        public RequestContext GetRequestContext(string url)
+            => GetRequestContext(new Uri(url));
 
         #region CanHandle
 
@@ -46,6 +58,18 @@ namespace MoEmbed.Providers
         }
 
         [Theory]
+        [InlineData("https://twitter.com/foo/status/560070183650213889", 560070183650213889L, "Twitter")]
+        public async void GetMetadataTest_CorrectScreenName(string uri, long statusId, string screenName)
+        {
+            var m = Assert.IsType<TwitterMetadata>(Provider.GetMetadata(new ConsumerRequest(new Uri(uri))));
+
+            await m.FetchAsync(GetRequestContext(uri));
+            Assert.Equal(statusId, m.TweetId);
+            Assert.Equal(screenName, m.ScreenName);
+            Assert.NotNull(m.Data);
+        }
+
+        [Theory]
         [InlineData("https://rms.sexy/")]
         public void GetMetadataTest_Null(string uri)
             => Assert.Null(Provider.GetMetadata(new ConsumerRequest(new Uri(uri))));
@@ -53,10 +77,3 @@ namespace MoEmbed.Providers
         #endregion GetMetadata
     }
 }
-
-
-
-
-
-
-
