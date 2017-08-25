a0850bf3a25e4c0713ed38938b3ee8f07175c310
pgrho <shipwreck@live.jp>
2017-08-25 22:28:56 +0900

    Added ResponseWriterExtensionTest (fail)

---
+++ MoEmbed.Core.Tests/Models/ResponseWriterExtensionTest.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using MoEmbed.Models.OEmbed;
+using Xunit;
+
+namespace MoEmbed.Models
+{
+    public class ResponseWriterExtensionTest
+    {
+        #region WriteOEmbedTest
+
+        [Theory]
+        [InlineData(Types.Link)]
+        [InlineData(Types.Photo)]
+        [InlineData(Types.Rich)]
+        [InlineData(Types.Video)]
+        public void WriteOEmbedTest_Json(Types type)
+        {
+            using (var ms = new MemoryStream())
+            {
+                new JsonResponseWriter(ms).WriteOEmbed(new OEmbedData() { Type = type });
+            }
+        }
+
+        [Theory]
+        [InlineData(Types.Link)]
+        [InlineData(Types.Photo)]
+        [InlineData(Types.Rich)]
+        [InlineData(Types.Video)]
+        public void WriteOEmbedTest_Xml(Types type)
+        {
+            using (var ms = new MemoryStream())
+            {
+                new XmlResponseWriter(ms).WriteOEmbed(new OEmbedData() { Type = type });
+            }
+        }
+
+        #endregion WriteOEmbedTest
+
+        #region WriteEmbedDataTest
+
+        [Fact]
+        public void WriteEmbedDataTest_Json()
+        {
+            using (var ms = new MemoryStream())
+            {
+                new JsonResponseWriter(ms).WriteEmbedData(new EmbedData());
+            }
+        }
+
+        [Fact]
+        public void WriteEmbedDataTest_Xml()
+        {
+            using (var ms = new MemoryStream())
+            {
+                new XmlResponseWriter(ms).WriteEmbedData(new EmbedData());
+            }
+        }
+
+        #endregion WriteEmbedDataTest
+    }
+}
