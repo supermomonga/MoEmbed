402c0d2f2180ee7406ad77b85059af3335772861
supermomonga <hi@supermomonga.com>
2017-08-30 10:42:25 +0900

    Remove unnecessary using

--- MoEmbed.Core/Models/Metadata/PixivMetadata.cs
+++ MoEmbed.Core/Models/Metadata/PixivMetadata.cs
@@ -1,11 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using HtmlAgilityPack;
 
 namespace MoEmbed.Models.Metadata
