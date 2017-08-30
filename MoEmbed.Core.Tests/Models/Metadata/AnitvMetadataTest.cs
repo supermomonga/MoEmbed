0646c7c6a98e0cb1827e435a6732b720b0dc3523
pgrho <shipwreck@live.jp>
2017-08-30 20:54:52 +0900

    test

--- MoEmbed.Core.Tests/Models/Metadata/AnitvMetadataTest.cs
+++ MoEmbed.Core.Tests/Models/Metadata/AnitvMetadataTest.cs
@@ -18,24 +18,30 @@ namespace MoEmbed.Models.Metadata
         [InlineData("https://ch.ani.tv/episodes/6501", "ゆるゆり なちゅやちゅみ！+　+1")]
         public void LoadHtml_TitleTest(string url, string title)
         {
-            var t = new AnitvMetadata() { Uri = url };
-
             using (var hc = new HttpClient())
             {
                 var html = hc.GetAsync(url).GetAwaiter().GetResult().Content.ReadAsStringAsync().GetAwaiter().GetResult();
 
+                var t = new AnitvMetadata() { Uri = url };
                 var hd = t.LoadHtml(html);
                 var nav = hd.CreateNavigator();
                 var d = t.Data;
                 try
                 {
                     Assert.Equal(title, d.Title);
+
+                    var t2 = new AnitvMetadata() { Uri = url };
+                    var d2 = t2.FetchAsync(new RequestContext(new MetadataService(), new ConsumerRequest(url.ToUri()))).GetAwaiter().GetResult();
+
+                    Assert.Equal(title, d2.Title);
                 }
                 catch
                 {
                     Output.WriteLine("h3:" + nav.SelectSingleNode("//*[@class='movie-content-note']/h3/text()")?.Value);
                     Output.WriteLine("h2:" + nav.SelectSingleNode("//*[@class='movie-content-note']/h2/text()")?.Value);
                     Output.WriteLine("HTML: " + html);
+
+                    throw;
                 }
             }
         }
