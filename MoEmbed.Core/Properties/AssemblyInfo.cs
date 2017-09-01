d0cea46a79b11824c672dad0c5136cba2bec2052
pgrho <shipwreck@live.jp>
2017-09-01 20:13:47 +0900

    Added XMLNS

--- MoEmbed.Core/Properties/AssemblyInfo.cs
+++ MoEmbed.Core/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
+using Portable.Xaml.Markup;
 
 [assembly: InternalsVisibleTo("MoEmbed.Core.Tests")]
+[assembly: XmlnsPrefix("https://moembed.darui.io/", "mo")]
+[assembly: XmlnsDefinition("https://moembed.darui.io/", "MoEmbed.Models.Metadata")]
