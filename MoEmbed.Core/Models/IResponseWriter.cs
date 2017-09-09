2463a7cc9ef1561c7aab5976f79302cf6e1d29a9
pgrho <shipwreck@live.jp>
2017-09-09 19:38:54 +0900

    Added IResponseWriter comments

--- MoEmbed.Core/Models/IResponseWriter.cs
+++ MoEmbed.Core/Models/IResponseWriter.cs
@@ -2,22 +2,118 @@ using System;
 
 namespace MoEmbed.Models
 {
-  public interface IResponseWriter : IDisposable
-  {
-    void WriteStartResponse(string name = "embed");
-    void WriteStartArrayProperty(string name);
-    void WriteEndArrayProperty();
-    void WriteStartObjectProperty(string name);
-    void WriteEndObjectProperty();
-    void WriteProperty(string name, bool value);
-    void WriteProperty(string name, int value);
-    void WriteProperty(string name, double value);
-    void WriteProperty(string name, object value);
-    void WriteStartObject(string name);
-    void WriteEndObject();
-    void WriteArrayValue(string name, bool value);
-    void WriteArrayValue(string name, double value);
-    void WriteArrayValue(string name, object value);
-    void WriteEndResponse();
-  }
+    /// <summary>
+    /// Provides methods to write a response.
+    /// </summary>
+    public interface IResponseWriter : IDisposable
+    {
+        #region Response
+
+        /// <summary>
+        /// Writes a start of the response.
+        /// </summary>
+        /// <param name="name">The name of the response element.</param>
+        void WriteStartResponse(string name = "embed");
+
+        /// <summary>
+        /// Writes an end of the response.
+        /// </summary>
+        void WriteEndResponse();
+
+        #endregion Response
+
+        #region Complex Property
+
+        /// <summary>
+        /// Writes a property name and start of an array.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        void WriteStartArrayProperty(string name);
+
+        /// <summary>
+        /// Writes a end of current array property.
+        /// </summary>
+        void WriteEndArrayProperty();
+
+        /// <summary>
+        /// Writes a property name and start of an object.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        void WriteStartObjectProperty(string name);
+
+        /// <summary>
+        /// Writes a end of current object property.
+        /// </summary>
+        void WriteEndObjectProperty();
+
+        #endregion Complex Property
+
+        #region Primitive Property
+
+        /// <summary>
+        /// Writes a property with boolean value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
+        void WriteProperty(string name, bool value);
+
+        /// <summary>
+        /// Writes a property with integer value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
+        void WriteProperty(string name, int value);
+
+        /// <summary>
+        /// Writes a property with float value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
+        void WriteProperty(string name, double value);
+
+        /// <summary>
+        /// Writes a property with string value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
+        void WriteProperty(string name, object value);
+
+        #endregion Primitive Property
+
+        #region Array Element
+
+        /// <summary>
+        /// Writes a start of an object.
+        /// </summary>
+        /// <param name="name">The name of the object.</param>
+        void WriteStartObject(string name);
+
+        /// <summary>
+        /// Writes a end of current object.
+        /// </summary>
+        void WriteEndObject();
+
+        /// <summary>
+        /// Writes a array element with the boolean value.
+        /// </summary>
+        /// <param name="name">The name of the element.</param>
+        /// <param name="value">The value of the element.</param>
+        void WriteArrayValue(string name, bool value);
+
+        /// <summary>
+        /// Writes a array element with the float value.
+        /// </summary>
+        /// <param name="name">The name of the element.</param>
+        /// <param name="value">The value of the element.</param>
+        void WriteArrayValue(string name, double value);
+
+        /// <summary>
+        /// Writes a array element with the string value.
+        /// </summary>
+        /// <param name="name">The name of the element.</param>
+        /// <param name="value">The value of the element.</param>
+        void WriteArrayValue(string name, object value);
+
+        #endregion Array Element
+    }
 }
--- MoEmbed.Core/Models/XmlResponseWriter.cs
+++ MoEmbed.Core/Models/XmlResponseWriter.cs
@@ -5,15 +5,34 @@ using System.Xml;
 
 namespace MoEmbed.Models
 {
-    public class XmlResponseWriter : IResponseWriter
+    /// <summary>
+    /// Provides methods to write a response in XML format.
+    /// </summary>
+    public sealed class XmlResponseWriter : IResponseWriter
     {
         private readonly bool _LeaveOpen;
 
+        private XmlWriter BaseWriter;
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlResponseWriter" /> with specified <see
+        /// cref="Stream" />.
+        /// </summary>
+        /// <param name="stream">The <see cref="Stream" /> to write response.</param>
+        /// <param name="leaveOpen">A value indicating whether the writer disposes base steam.</param>
         public XmlResponseWriter(Stream stream, bool leaveOpen = false)
         {
             BaseWriter = XmlWriter.Create(new StreamWriter(stream, new UTF8Encoding(false), 4096, leaveOpen));
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlResponseWriter" /> with specified <see
+        /// cref="TextWriter" />.
+        /// </summary>
+        /// <param name="textWriter">The <see cref="TextWriter" /> to write response.</param>
+        /// <param name="leaveOpen">A value indicating whether the writer disposes base writer.</param>
         public XmlResponseWriter(TextWriter textWriter, bool leaveOpen = false)
         {
             BaseWriter = XmlWriter.Create(textWriter, new XmlWriterSettings()
@@ -22,27 +41,117 @@ namespace MoEmbed.Models
             });
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlResponseWriter" /> with specified <see
+        /// cref="XmlWriter" />.
+        /// </summary>
+        /// <param name="baseWriter">The <see cref="XmlWriter" /> to write response.</param>
+        /// <param name="leaveOpen">A value indicating whether the writer disposes base writer.</param>
         public XmlResponseWriter(XmlWriter baseWriter, bool leaveOpen = false)
         {
             BaseWriter = baseWriter;
             _LeaveOpen = leaveOpen;
         }
-        protected XmlWriter BaseWriter { get; private set; }
 
+        #endregion Constructors
+
+        #region Response
+
+        /// <summary>
+        /// Writes a start of the response.
+        /// </summary>
+        /// <param name="name">The name of the response element.</param>
         public void WriteStartResponse(string name)
         {
             ThrowIfDisposed();
             BaseWriter.WriteStartElement(name);
         }
 
+        /// <summary>
+        /// Writes an end of the response.
+        /// </summary>
+        public void WriteEndResponse()
+        {
+            ThrowIfDisposed();
+            BaseWriter.WriteEndElement();
+            BaseWriter.Flush();
+        }
+
+        #endregion Response
+
+        #region Complex Property
+
+        /// <summary>
+        /// Writes a property name and start of an array.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        public void WriteStartArrayProperty(string name)
+        {
+            ThrowIfDisposed();
+            BaseWriter.WriteStartElement(name);
+        }
+
+        /// <summary>
+        /// Writes a end of current array property.
+        /// </summary>
+        public void WriteEndArrayProperty()
+        {
+            ThrowIfDisposed();
+            BaseWriter.WriteEndElement();
+        }
+
+        /// <summary>
+        /// Writes a property name and start of an object.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        public void WriteStartObjectProperty(string name)
+        {
+            ThrowIfDisposed();
+            BaseWriter.WriteStartElement(name);
+        }
+
+        /// <summary>
+        /// Writes a end of current object property.
+        /// </summary>
+        public void WriteEndObjectProperty()
+        {
+            ThrowIfDisposed();
+            BaseWriter.WriteEndElement();
+        }
+
+        #endregion Complex Property
+
+        #region Primitive Property
+
+        /// <summary>
+        /// Writes a property with boolean value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
         public void WriteProperty(string name, bool value)
             => WriteProperty(name, value ? "true" : "false");
 
+        /// <summary>
+        /// Writes a property with integer value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
         public void WriteProperty(string name, int value)
             => WriteProperty(name, value.ToString());
 
+        /// <summary>
+        /// Writes a property with float value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
         public void WriteProperty(string name, double value)
             => WriteProperty(name, value.ToString("r"));
+
+        /// <summary>
+        /// Writes a property with string value.
+        /// </summary>
+        /// <param name="name">The property name to write.</param>
+        /// <param name="value">The value of the property.</param>
 
         public void WriteProperty(string name, object value)
         {
@@ -52,55 +161,50 @@ namespace MoEmbed.Models
             BaseWriter.WriteEndElement();
         }
 
-        public void WriteEndResponse()
-        {
-            ThrowIfDisposed();
-            BaseWriter.WriteEndElement();
-            BaseWriter.Flush();
-        }
+        #endregion Primitive Property
 
-        public void WriteStartArrayProperty(string name)
-        {
-            ThrowIfDisposed();
-            BaseWriter.WriteStartElement(name);
-        }
+        #region Array Element
 
-        public void WriteEndArrayProperty()
-        {
-            ThrowIfDisposed();
-            BaseWriter.WriteEndElement();
-        }
-
-        public void WriteStartObjectProperty(string name)
-        {
-            ThrowIfDisposed();
-            BaseWriter.WriteStartElement(name);
-        }
-
-        public void WriteEndObjectProperty()
-        {
-            ThrowIfDisposed();
-            BaseWriter.WriteEndElement();
-        }
-
+        /// <summary>
+        /// Writes a start of an object.
+        /// </summary>
+        /// <param name="name">The name of the object.</param>
         public void WriteStartObject(string name)
         {
             ThrowIfDisposed();
             BaseWriter.WriteStartElement(name);
         }
 
+        /// <summary>
+        /// Writes a end of current object.
+        /// </summary>
         public void WriteEndObject()
         {
             ThrowIfDisposed();
             BaseWriter.WriteEndElement();
         }
 
+        /// <summary>
+        /// Writes a array element with the boolean value.
+        /// </summary>
+        /// <param name="name">The name of the element.</param>
+        /// <param name="value">The value of the element.</param>
         public void WriteArrayValue(string name, bool value)
             => WriteArrayValue(name, value ? "true" : "false");
 
+        /// <summary>
+        /// Writes a array element with the float value.
+        /// </summary>
+        /// <param name="name">The name of the element.</param>
+        /// <param name="value">The value of the element.</param>
         public void WriteArrayValue(string name, double value)
             => WriteArrayValue(name, value.ToString("r"));
 
+        /// <summary>
+        /// Writes a array element with the string value.
+        /// </summary>
+        /// <param name="name">The name of the element.</param>
+        /// <param name="value">The value of the element.</param>
         public void WriteArrayValue(string name, object value)
         {
             ThrowIfDisposed();
@@ -109,14 +213,11 @@ namespace MoEmbed.Models
             BaseWriter.WriteEndElement();
         }
 
-        private void ThrowIfDisposed()
-        {
-            if (BaseWriter == null)
-            {
-                throw new ObjectDisposedException(nameof(BaseWriter));
-            }
-        }
+        #endregion Array Element
 
+        /// <summary>
+        /// Provides a mechanism for releasing unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
             if (!_LeaveOpen)
@@ -124,6 +225,14 @@ namespace MoEmbed.Models
                 BaseWriter?.Dispose();
             }
             BaseWriter = null;
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (BaseWriter == null)
+            {
+                throw new ObjectDisposedException(nameof(BaseWriter));
+            }
         }
     }
 }
